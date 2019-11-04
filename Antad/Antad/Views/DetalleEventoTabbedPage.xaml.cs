01614a94c50fe34d93109b0511727f2b2975ada6
avidaurri <40777508+avidaurri@users.noreply.github.com>
2019-11-04 08:48:37 -0600

    lunes 04112019 ini

--- Antad/Antad/Views/DetalleEventoTabbedPage.xaml.cs
+++ Antad/Antad/Views/DetalleEventoTabbedPage.xaml.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace Antad.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class DetalleEventoTabbedPage : TabbedPage
+    public partial class DetalleEventoTabbedPage : Xamarin.Forms.TabbedPage
     {
         public DetalleEventoTabbedPage()
         {
             InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
     }
 }
